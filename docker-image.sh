ffb20fae6afa8066049bb757d5aeb1693da2c3af
Arief-Karditya <nolifegansaya@gmail.com>
2023-12-30 21:05:06 +0700

    Docker Image, Image Pulling

--- docker-image.sh
+++ docker-image.sh
@@ -1,2 +1,16 @@
 # Command Melihat Docker Image
 docker image ls
+
+# Command Download Docker Image dari Docker Registry
+# docker image pull < tag nama software image > : < tag versi software image >
+
+# Contoh pull Docker Image redis:
+docker image pull redis:latest
+
+# Contoh pull Docker Image mysql:
+docker image pull mysql:latest
+
+# Tag ":latest" akan mengunduh Docker Image dengan 
+# versi software paling baru dari Docker Registry
+# Tag bisa diubah sesuai dengan Tag di halaman dokumentasi
+# Docker Image Terkait
