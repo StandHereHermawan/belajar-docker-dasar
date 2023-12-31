d7677d479e94b6ee0df3a717e42385cb88f698c2
Arief-Karditya <nolifegansaya@gmail.com>
2023-12-31 19:09:01 +0700

    Docker Container, Melihat Container

---
+++ docker-container.sh
@@ -0,0 +1,8 @@
+# Command melihat semua Docker container 
+# baik yang berjalan ataupun tidak berjalan
+# di Docker Daemon
+docker container ls -a
+
+# Command melihat semua Docker container
+# hanya yang berjalan saja
+docker container ls
