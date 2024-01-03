de90d70cac0b5c55e1a55c7fe74a95430b16ada3
Arief-Karditya <nolifegansaya@gmail.com>
2024-01-03 14:58:37 +0700

    Container Port, Port Forwarding

---
+++ docker-container-port-forwarding.sh
@@ -0,0 +1,14 @@
+# Mengambil Image Nginx untuk Keperluan Praktek
+docker image pull nginx:latest
+
+# Untuk melakukan port forwarding, 
+# kita bisa menggunakan perintah berikut ketika membuat container nya :
+# docker container create --name < namacontainer > --publish < porthost >:< portcontainer > < image >:< tag >
+# Jika kita ingin melakukan port forwarding lebih dari satu, 
+# kita bisa tambahkan dua kali parameter --publish
+
+# Contoh Command untuk port forwarding Docker Container nginx
+docker container create --name contohNginx --publish 8080:80 nginx:latest
+
+# menjalankan Docker Container Nginx yang sudah di port forward
+docker container start contohNginx
